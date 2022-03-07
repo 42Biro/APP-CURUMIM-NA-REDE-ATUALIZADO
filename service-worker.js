e92f6f18026fd6e7aa3cd0464a189445d9ff246d
Lucas Carvalho <89099213+42Biro@users.noreply.github.com>
2022-03-06 22:37:16 -0300

    Atualizacao 04

--- service-worker.js
+++ service-worker.js
@@ -4,10 +4,10 @@
  * isso garante que a aplicação será atualizada nos clientes onde já exista
  * um cache salvo
  */
- const version = 6
- const cachename = 'app-cache-v'+version
- 
- /**
+const version = 9
+const cachename = 'app-cache-v'+version
+
+/**
   * Arquivos que serão salvos no cache para uso offline
   * O caminho para os arquivos deve ser completo e sem o dominio
   * Ex: arquivo logo.png
@@ -18,55 +18,72 @@
   *      URL: https://bpvifsc.github.io/template-app-pwa/index.html
   *      Caminho: ./imagens/logo.png
   */
- const arquivos = [
-     "./",
-     "./index.html",
-     "./main.js",
-     "./service-worker.js",
-     "./manifest.json",
-     "./estilos.css",
-     "./imagens/bootstrap.jpg",
-     "./imagens/ig-logo.svg",
-     "./imagens/linus-torvalds.jpg",
-     "./imagens/logo.png",
-     "./imagens/mail.svg",
-     "./imagens/tim-berners-lee.jpg",
-     "./imagens/wpp-logo.svg",
-     "./imagens/icone192.png",
-     "./imagens/icone512.png"
-   ]
- 
-   /**
+const arquivos = [
+      "./",
+      "./index.html",
+      "./main.js",
+      "./service-worker.js",
+      "./manifest.json",
+      "./estilos.css",
+      "./imagens/bootstrap.jpg",
+      "./imagens/ig-logo.svg",
+      "./imagens/linus-torvalds.jpg",
+      "./imagens/logo.png",
+      "./imagens/mail.svg",
+      "./imagens/tim-berners-lee.jpg",
+      "./imagens/wpp-logo.svg",
+      "./imagens/icone192.png",
+      "./imagens/icone512.png",
+      "./imagens/cachoeira01.jpg",
+      "./imagens/cachoeira02.jpg",
+      "./imagens/cachoeira03.jpg",
+      "./imagens/cachoeiramaruaga01.jpeg",
+      "./imagens/cachoeiramaruaga02.jpeg",
+      "./imagens/cachoeiramaruaga03.jpeg",
+      "./imagens/cachoeirasantuario01.jpeg",
+      "./imagens/cachoeirasantuario02.jpeg",
+      "./imagens/cachoeirasantuario03.jpeg",
+      "./imagens/cachoeiraneblina01.jpeg",
+      "./imagens/cachoeiraneblina02.jpeg",
+      "./imagens/cachoeiraneblina03.jpeg",
+      "./imagens/homelogo.jpeg",
+      "./imagens/homelogo.png",
+      "./imagens/icon01.jpeg",
+      "./imagens/icon02.jpeg",
+      "./imagens/icon03.jpeg",
+      "./imagens/icon04.jpeg",
+      "./imagens/icon05.jpeg",
+]
+/**
     * Cria o cache dos arquivos
     */
-   self.addEventListener('install', function(event) {
-     event.waitUntil(
-       caches.open(cachename).then(function(cache) {
-         return cache.addAll(arquivos);
-       })
-     );
-   });
-   
-   /**
+self.addEventListener('install', function(event) {
+event.waitUntil(
+caches.open(cachename).then(function(cache) {
+return cache.addAll(arquivos);
+})
+);
+});
+/**
     * Verifica se existe uma versão em cache das páginas
     * Caso não seja possivel retorna o match(index) da catch
     * Está página pode ser tratada e retornar uma mensagem de erro/offline
     */
-   self.addEventListener('fetch', function(event) {
-     event.respondWith(caches.match(event.request).then(function(response) {
-       if (response !== undefined) {
-         return response;
-       } else {
-         return fetch(event.request).then(function (response) {
-           let responseClone = response.clone();
-           
-           caches.open(cachename).then(function (cache) {
-             cache.put(event.request, responseClone);
-           });
-           return response;
-         }).catch(function () {
-           return caches.match('./index.html');
-         });
-       }
-     }));
-   });
+self.addEventListener('fetch', function(event) {
+event.respondWith(caches.match(event.request).then(function(response) {
+if (response !== undefined) {
+return response;
+} else {
+return fetch(event.request).then(function (response) {
+let responseClone = response.clone();
+
+caches.open(cachename).then(function (cache) {
+cache.put(event.request, responseClone);
+});
+return response;
+}).catch(function () {
+return caches.match('./index.html');
+});
+}
+}));
+});
